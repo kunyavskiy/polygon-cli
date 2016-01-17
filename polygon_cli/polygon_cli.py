4595851bc073861205ed88a770587cdfb774f853
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2016-01-17 19:19:40 +0300

    support running in src/solutions directory

--- polygon_cli/polygon_cli.py
+++ polygon_cli/polygon_cli.py
@@ -24,7 +24,13 @@ def fatal(error):
 
 def load_session():
     try:
-        session_data_json = open(config.get_session_file_path(), 'r').read()
+        if os.path.exists(config.get_session_file_path()):
+            session_data_json = open(config.get_session_file_path(), 'r').read()
+        elif os.path.exists(os.path.join('..', config.get_session_file_path())):
+            config.internal_directory_path = os.path.join('..', config.internal_directory_path)
+            session_data_json = open(config.get_session_file_path(), 'r').read()
+        else:
+            return False
         session_data = json.loads(session_data_json, object_hook=json_encoders.my_json_decoder)
         global_vars.problem = ProblemSession(config.polygon_url, session_data["problemId"])
         global_vars.problem.use_ready_session(session_data)
