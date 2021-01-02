37e6b9b4a20ce0fa6d6e16bdf784a22d1414598f
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2021-01-03 01:20:57 +0300

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name='polygon-cli',
-        version='1.1.6',
+        version='1.1.7',
         packages=['polygon_cli', 'polygon_cli.actions'],
         url='https://github.com/kunyavskiy/polygon-cli',
         license='MIT',
