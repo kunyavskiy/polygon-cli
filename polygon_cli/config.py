41ad6f2baf959e3f95ca2bea1ebb3f1d1ed31606
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2021-01-03 00:46:40 +0300

    support several polygons in auth file

--- polygon_cli/config.py
+++ polygon_cli/config.py
@@ -3,36 +3,7 @@ import sys
 import getpass
 import yaml
 
-
-polygon_url = "https://polygon.codeforces.com"
-
-
-authentication_file = os.path.join(os.path.expanduser('~'), '.config', 'polygon-cli', 'auth.yaml')
-if os.path.exists(authentication_file):
-    with open(authentication_file, 'r') as fo:
-        auth_data = yaml.load(fo, Loader=yaml.BaseLoader)
-    login = auth_data.get('login')
-    password = auth_data.get('password')
-    api_key = auth_data.get('api_key')
-    api_secret = auth_data.get('api_secret')
-
-if not os.path.exists(authentication_file) or not login or not api_key or not api_secret:
-    print('WARNING: Authentication data will be stored in plain text in {}'.format(authentication_file))
-    login = input('Login: ')
-    password = getpass.getpass('Password (leave blank if you want to enter it when needed): ')
-    api_key = input('API Key: ')
-    api_secret = input('API Secret: ')
-    os.makedirs(os.path.dirname(authentication_file), exist_ok=True)
-    with open(authentication_file, 'w') as fo:
-        auth_data = {
-            'login': login,
-            'password': password,
-            'api_key': api_key,
-            'api_secret': api_secret
-        }
-        yaml.dump(auth_data, fo, default_flow_style=False)
-    print('Authentication data is stored in {}'.format(authentication_file))
-
+MAIN_POLYGON_URL = 'https://polygon.codeforces.com'
 
 internal_directory_path = '.polygon-cli'
 default_source_types = {
@@ -75,3 +46,64 @@ def get_merge_tool(old, our, theirs):
 
 def get_diff_tool(old, our, theirs):
     return ' '.join(["diff", theirs, our])
+
+
+def setup_login_by_url(polygon_name):
+    global polygon_url, login, password, api_key, api_secret
+    authentication_file = os.path.join(os.path.expanduser('~'), '.config', 'polygon-cli', 'auth.yaml')
+    if os.path.exists(authentication_file):
+        with open(authentication_file, 'r') as fo:
+            auth_data = yaml.load(fo, Loader=yaml.BaseLoader)
+        if auth_data.get('version') is None:
+            with open(authentication_file, 'w') as fo:
+                auth_data = {
+                    'version': 1,
+                    'polygons': {
+                        'main': {
+                            'url': MAIN_POLYGON_URL,
+                            'login': auth_data.get('login'),
+                            'password': auth_data.get('password'),
+                            'api_key': auth_data.get('api_key'),
+                            'api_secret': auth_data.get('api_secret'),
+                        }
+                    }
+                }
+                yaml.dump(auth_data, fo, default_flow_style=False)
+        auth_data_by_name = auth_data.get('polygons').get(polygon_name)
+        if auth_data_by_name:
+            polygon_url = auth_data_by_name.get('url')
+            login = auth_data_by_name.get('login')
+            password = auth_data_by_name.get('password')
+            api_key = auth_data_by_name.get('api_key')
+            api_secret = auth_data_by_name.get('api_secret')
+
+    if not login or not api_key or not api_secret:
+        print('WARNING: Authentication data will be stored in plain text in {}'.format(authentication_file))
+        if polygon_name != 'main':
+            polygon_url = input('Url:')
+        else:
+            polygon_url = MAIN_POLYGON_URL
+        login = input('Login: ')
+        password = getpass.getpass('Password (leave blank if you want to enter it when needed): ')
+        api_key = input('API Key: ')
+        api_secret = input('API Secret: ')
+        os.makedirs(os.path.dirname(authentication_file), exist_ok=True)
+        with open(authentication_file, 'w') as fo:
+            auth_data['polygons'][polygon_name] = {
+                'url': polygon_url,
+                'login': login,
+                'password': password,
+                'api_key': api_key,
+                'api_secret': api_secret
+            }
+            yaml.dump(auth_data, fo, default_flow_style=False)
+        print('Authentication data is stored in {}'.format(authentication_file))
+
+
+polygon_url = None
+login = None
+password = None
+api_key = None
+api_secret = None
+
+setup_login_by_url('main')
