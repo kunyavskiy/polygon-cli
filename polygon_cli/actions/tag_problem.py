59dcb7bcfe26fd2215c56cbce1b1b33ac6a01bb1
i1oveMyse1f <sapozhnikov01@mail.ru>
2021-09-09 22:43:25 +0300

    Add help in add_argument

--- polygon_cli/actions/tag_problem.py
+++ polygon_cli/actions/tag_problem.py
@@ -10,10 +10,10 @@ def process_tag_problem(tags):
 def add_parser(subparsers):
     parser_add = subparsers.add_parser(
         'tag_problem',
-        help="Saves tags for the problem. Existed tags will be replaced by new tags.\n"
-        "Parameters: Non-empty sequnce of tags. If you specified several same tags will be add only one of them."
+        help="Saves tags for the problem. Existed tags will be replaced by new tags"
     )
-    parser_add.add_argument('tags', nargs='+')
+    parser_add.add_argument('tags', nargs='+', 
+                            help="Non-empty sequnce of tags. If you specified several same tags will be add only one of them")
 
     def read_options(options):
         if not load_session_with_options(options):
