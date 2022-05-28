3a474e05a123240b92da03c668ac4720a6f4e8d4
Niyaz Nigmatullin <niyaznigmatullin@github.com>
2022-05-28 13:51:48 +0300

    Add long_description for pypi, bump version to 1.1.11

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
         name='polygon-cli',
-        version='1.1.10',
+        version='1.1.11',
         packages=['polygon_cli', 'polygon_cli.actions'],
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         url='https://github.com/kunyavskiy/polygon-cli',
         license='MIT',
         author='Pavel Kunyavskiy',
